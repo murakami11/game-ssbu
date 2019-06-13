2833f524bc1773ed3b9c5ea10dabc2a62bca29ce
taro.kozima <p4vg123@gmail.com>
2019-06-13 07:32:48 +0000

    本番と開発とで場合分けを追加

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -4,10 +4,12 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   if Rails.env.production?
     include Cloudinary::CarrierWave
+  else
+    storage :file
   end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
